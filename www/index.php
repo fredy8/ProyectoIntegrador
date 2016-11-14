b856f69eebd0dad9b741c2dd91dfe81f415e760c
fredy8 <fredy.altamirano8@gmail.com>
2016-11-13 20:46:15 -0600

    connect to mysql

--- www/index.php
+++ www/index.php
@@ -6,5 +6,20 @@
 echo "My first PHP script!";
 ?>
 
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "1qazxsw2";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
+
 </body>
 </html>
