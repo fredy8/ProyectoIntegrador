ad2f65f4ee6c800f14dda1664ce333d462beffd9
Alberto Astiazaran <pmdmaster@hotmail.com>
2016-11-16 23:43:10 -0600

    Create school table query.

--- sql/schema.sql
+++ sql/schema.sql
@@ -7,3 +7,17 @@ create table users (
   password varchar(61) not null,
   approved bool default false
 );
+
+create table escuelas (
+  id bigint(20) unsigned not null auto_increment primary key,
+  nombre varchar(255) not null unique,
+  director varchar(100) not null,
+  nivel varchar(30) not null,
+  turno varchar(30) not null,
+  sostenimiento varchar(30) not null,
+  direccion varchar(255) not null,
+  region varchar(30) not null,
+  fecha date not null,
+  alumnos INTEGER not null,
+  comentarios varchar(500) not null
+);
